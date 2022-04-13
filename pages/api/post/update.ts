50066d91bbab9196ab22873ff435c6e5871fa525
Matthew Lee <matthew.j.lee@shopify.com>
2022-04-12 22:37:34 -0400

    operand update

--- pages/api/post/update.ts
+++ pages/api/post/update.ts
@@ -1,14 +1,22 @@
 import prisma from '../../../lib/prisma';
+import operand from 'lib/operand'
 
 export default async function handler(req, res) {
   const id = req.body.id
   const data = req.body.data
   console.log(`[update] received id: ${id}`)
-  await prisma.post.update({
+  const post = await prisma.post.update({
     where: {
       id: id 
     },
     data: data
+  })
+  operand.updateGroup(post.operandId, {
+    kind: "html",
+    metadata: {
+      html: post.question,
+      title: post.answer ? post.answer : post.question,
+    }
   })
   res.status(200).json({ id: id })
 }
